1bbd32de9ad039ff9fb97f05a8ce6397d7a24eb8
Lucien Najev <luciennajev@hotmail.com>
2019-01-28 22:14:24 +0000

    new ex

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -3,12 +3,7 @@ module Pangram
   ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
 
   def self.pangram?(sentence)
-    x = {}
-    sentence.downcase.scan(/[a-zA-Z]/).each do |letter|
-      x.key?(letter) ? x[letter] += 1 : x[letter] = 1
-    end
-
-    true if x.size == ALPHABET.length
+    sentence.downcase.scan(/[a-z]/).uniq.count == ALPHABET.length
   end
 
 end
