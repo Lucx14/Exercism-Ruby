514b0d70bfe360f77a50a7420051390404871bf7
Lucien Najev <luciennajev@hotmail.com>
2019-01-29 22:46:29 +0000

    new ex

---
+++ space-age/space_age.rb
@@ -0,0 +1,73 @@
+class SpaceAge
+
+  ORBITAL_PERIOD = {
+    :Earth => 1,
+    :Mercury => 0.2408467,
+    :Venus => 0.61519726,
+    :Mars => 1.8808158,
+    :Jupiter => 11.862615,
+    :Saturn => 29.447498,
+    :Uranus => 84.016846,
+    :Neptune => 164.79132
+  }
+  SECONDS_IN_YEAR = 31557600
+
+  attr_reader :seconds
+
+  def initialize(seconds)
+    @seconds = seconds
+  end
+
+  def on_earth
+    age(:Earth)
+  end
+
+  def on_mercury
+    age(:Mercury)
+  end
+
+  def on_venus
+    age(:Venus)
+  end
+
+  def on_mars
+    age(:Mars)
+  end
+
+  def on_jupiter
+    age(:Jupiter)
+  end
+
+  def on_saturn
+    age(:Saturn)
+  end
+
+  def on_jupiter
+    age(:Jupiter)
+  end
+
+  def on_uranus
+    age(:Uranus)
+  end
+
+  def on_neptune
+    age(:Neptune)
+  end
+
+  private
+
+  def days_to_seconds(years)
+    # 31557600 seconds
+    # p years * SpaceAge::SECONDS_IN_YEAR
+    # p years * 365.25 * 24 * 60 * 60
+    # p years * SpaceAge::SECONDS_IN_YEAR
+    years * 365.25 * 24 * 60 * 60
+    # years * SpaceAge::SECONDS_IN_YEAR
+  end
+
+  def age(planet)
+    # p seconds/days_to_seconds(SpaceAge::ORBITAL_PERIOD[planet])
+    seconds/days_to_seconds(SpaceAge::ORBITAL_PERIOD[planet])
+  end
+
+end
