d03e05fa1bc4187e217c0b49391ec0bdb05ac7e7
Lucien Najev <luciennajev@hotmail.com>
2019-06-14 12:30:03 +0100

    new ex

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -1,13 +1,20 @@
 class Phrase
 
-  attr_reader :sentence
-
   def initialize(sentence)
     @sentence = sentence
   end
 
   def word_count
-    {"word"=>1}
+    word_count = {}
+    sentence.lstrip.downcase.split(/[^'\w]+/).each do |word|
+      word.gsub!(/\A'|'\Z/, '')
+      word_count.has_key?(word) ? word_count[word] += 1 : word_count[word] = 1
+    end
+    word_count
   end
+
+  private
+
+  attr_reader :sentence
 
 end
