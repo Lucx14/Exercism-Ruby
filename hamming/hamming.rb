5617e2b6d6adc6d87610e117e98b83d4f5aa6d0d
Lucien Najev <luciennajev@hotmail.com>
2019-07-07 12:06:14 +0100

    refactor

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -2,9 +2,6 @@ module Hamming
   ERROR_MESSAGE = "Error: both strands must be of the same length"
   def self.compute(strand1, strand2)
     raise ArgumentError.new(ERROR_MESSAGE) if strand1.length != strand2.length
-    count = 0
-    n = strand1.length - 1
-    (0..n).each { |i| count += 1 if strand1[i] != strand2[i] }
-    count
+    (0...strand1.length).count { |i| strand1[i] != strand2[i] }
   end
 end
