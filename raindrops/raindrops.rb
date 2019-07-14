1da6b7b1fe75a5b473047614f3715a0194ff42a5
Lucien Najev <luciennajev@hotmail.com>
2019-07-14 13:35:50 +0100

    (new ex)

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,7 +1,7 @@
 module Raindrops
   MAP = { 3 => "Pling", 5 => "Plang", 7 => "Plong" }
-  def self.convert(arg)
-    arr = [3, 5, 7].select { |divisor| arg % divisor == 0 }.map { |i| MAP[i] }
-    arr.length == 0 ? arg.to_s : arr.join
+  def self.convert(number)
+    named_factors = MAP.keys.select { |divisor| number % divisor == 0 }.map { |i| MAP[i] }
+    named_factors.empty? ? number.to_s : named_factors.join
   end
 end
