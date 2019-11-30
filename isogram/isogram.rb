74eafcdcab856b7b2b62f369b79dfb58bc8174da
Lucien Najev <luciennajev@hotmail.com>
2019-11-30 10:48:05 +0000

    isogram refactor

--- isogram/isogram.rb
+++ isogram/isogram.rb
@@ -2,8 +2,7 @@ module Isogram
   extend self
 
   def isogram?(input)
-    letters = input.scan(/[a-z]/i).map(&:downcase)
-    letter_count = letters.group_by{ |element| element }.map{|k, v| [k, v.length]}.to_h
-    letter_count.select{|k,v| v > 1}.empty?
+    letters = input.downcase.scan(/[a-z]/i)
+    letters.uniq.count == letters.count
   end
 end
