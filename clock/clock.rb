de671beeb10c16969a0e9f01bd91dcdfd5a01146
Lucien Najev <luciennajev@hotmail.com>
2020-01-19 15:59:24 +0000

    refactored clock

--- clock/clock.rb
+++ clock/clock.rb
@@ -1,4 +1,6 @@
 class Clock
+  MINUTES_PER_HOUR = 60
+  MINUTES_PER_DAY = 1440
 
   attr_reader :hour, :minute
 
@@ -8,7 +10,8 @@ class Clock
   end
 
   def to_s
-    current_hour + ':' + current_minute
+    total_minutes.divmod(MINUTES_PER_HOUR)
+      .map{ |n| format_number(n)}.join(':')
   end
 
   def +(clock)
@@ -25,46 +28,8 @@ class Clock
 
   private
 
-  def clockface_hour(num)
-    if num < 24 && num >= 0
-      num
-    elsif num > 23
-      num % 24
-    elsif num < 0 && num > -25
-      24 + num
-    elsif num < -24
-      num % 24
-    end
-  end
-
-  def clockface_minute(num)
-    if num < 60 && num >=0
-      num
-    elsif num > 59
-      num % 60
-    elsif num > -60 && num < 0
-      60 + num
-    elsif num < -59
-      num % 60
-    end
-  end
-
-  def hour_increment(num)
-    if num > 59
-      (num / 60) > 24 ? (num / 60) - 24 : (num / 60)
-    elsif num < 0
-      (num / 60) < -24 ? (num / 60) + 24 : (num / 60)
-    else
-      0
-    end
-  end
-
-  def current_hour
-    format_number(clockface_hour(clockface_hour(hour) + hour_increment(minute)))
-  end
-
-  def current_minute
-    format_number(clockface_minute(minute))
+  def total_minutes
+    (hour * MINUTES_PER_HOUR + minute) % MINUTES_PER_DAY
   end
 
   def format_number(n)
