70e352e523959b5bab81082c503c53662e96a09d
Lucien Najev <luciennajev@hotmail.com>
2020-01-26 13:21:26 +0000

    refactor

--- clock/clock.rb
+++ clock/clock.rb
@@ -3,34 +3,37 @@ class Clock
   MINUTES_PER_DAY = 1440
   TIME_FORMAT = "%02d:%02d"
 
-  attr_reader :hour, :minute
+  attr_reader :time
 
   def initialize(hour: 0, minute: 0)
-    @hour = hour
-    @minute = minute
+    @time = total_minutes(hour, minute)
   end
 
   def to_s
-    TIME_FORMAT % total_minutes.divmod(MINUTES_PER_HOUR)
+    TIME_FORMAT % time.divmod(MINUTES_PER_HOUR)
   end
 
   def +(clock)
-    Clock.new(hour: (hour + clock.hour), minute: (minute + clock.minute))
+    Clock.new(hour: 0, minute: (time + clock.time))
   end
 
   def -(clock)
-    Clock.new(hour: (hour - clock.hour), minute: (minute - clock.minute))
+    Clock.new(hour: 0, minute: (time - clock.time))
   end
 
   def ==(clock)
     self.class == clock.class &&
-    to_s == clock.to_s
+    hash == clock.hash
+  end
+  alias :eql? :==
+
+  def hash
+    [@time].hash
   end
 
   private
 
-  def total_minutes
+  def total_minutes(hour, minute)
     (hour * MINUTES_PER_HOUR + minute) % MINUTES_PER_DAY
   end
 end
-
