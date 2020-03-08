596c215ef5c47d17381d2284c4bd0d73fed5fef6
Lucien Najev <luciennajev@hotmail.com>
2020-03-08 17:04:06 +0000

    new ex

--- tournament/tournament.rb
+++ tournament/tournament.rb
@@ -1,8 +1,64 @@
-module Tournament
-
+class Tournament
+ 
   def self.tally(input)
-    "Team                           | MP |  W |  D |  L |  P\n"
+    match_report = input.split("\n").map{|game| game.split(";")}
+    new(match_report).generate_results_table
   end
 
-end
+  def generate_results_table
+    calculate_results
+    format_results
+  end
 
+  private
+
+  TABLE_HEADER = "Team".ljust(31) + "| MP |  W |  D |  L |  P\n"
+
+  attr_reader :match_report
+  attr_accessor :results, :table
+
+  def initialize(match_report, results = {}, table = "")
+    @match_report = match_report
+    @results = results
+    @table = table << TABLE_HEADER
+  end
+
+  def calculate_results
+    match_report.each do |game|
+      convert_game_result(game)
+    end
+    @results = results.sort_by { |key, value| [-value[:P], key] }.to_h
+  end
+
+  def format_results
+    results.each do |key, value|
+      table << "#{key.ljust(31)}|  #{value[:MP]} |  #{value[:W]} |  #{value[:D]} |  #{value[:L]} |  #{value[:P]}\n"
+    end
+    table
+  end
+
+  def convert_game_result(game)
+    game.take(2).each do |team|
+      unless results.has_key?(team)
+        results[team] = { MP: 0, W: 0, D: 0, L: 0, P: 0 }
+      end
+    end
+
+    results[game[0]][:MP] += 1
+    results[game[1]][:MP] += 1
+    if game[2] == "win"
+      results[game[0]][:W] += 1
+      results[game[0]][:P] += 3
+      results[game[1]][:L] += 1
+    elsif game[2] == "loss"
+      results[game[0]][:L] += 1
+      results[game[1]][:W] += 1
+      results[game[1]][:P] += 3
+    elsif game[2] == "draw"
+      results[game[0]][:D] += 1
+      results[game[0]][:P] += 1
+      results[game[1]][:D] += 1
+      results[game[1]][:P] += 1
+    end
+  end
+end
--- tournament/tournament_test.rb
+++ tournament/tournament_test.rb
@@ -17,7 +17,7 @@ class TournamentTest < Minitest::Test
   end
 
   def test_a_win_is_three_points_a_loss_is_zero_points
-    skip
+    # skip
     input = <<~INPUT
     Allegoric Alaskans;Blithering Badgers;win
     INPUT
@@ -32,7 +32,7 @@ class TournamentTest < Minitest::Test
   end
 
   def test_a_win_can_also_be_expressed_as_a_loss
-    skip
+    # skip
     input = <<~INPUT
     Blithering Badgers;Allegoric Alaskans;loss
     INPUT
@@ -47,7 +47,7 @@ class TournamentTest < Minitest::Test
   end
 
   def test_a_different_team_can_win
-    skip
+    # skip
     input = <<~INPUT
     Blithering Badgers;Allegoric Alaskans;win
     INPUT
@@ -62,7 +62,7 @@ class TournamentTest < Minitest::Test
   end
 
   def test_a_draw_is_one_point_each
-    skip
+    # skip
     input = <<~INPUT
     Allegoric Alaskans;Blithering Badgers;draw
     INPUT
@@ -77,7 +77,7 @@ class TournamentTest < Minitest::Test
   end
 
   def test_there_can_be_more_than_one_match
-    skip
+    # skip
     input = <<~INPUT
     Allegoric Alaskans;Blithering Badgers;win
     Allegoric Alaskans;Blithering Badgers;win
@@ -93,7 +93,7 @@ class TournamentTest < Minitest::Test
   end
 
   def test_there_can_be_more_than_one_winner
-    skip
+    # skip
     input = <<~INPUT
     Allegoric Alaskans;Blithering Badgers;loss
     Allegoric Alaskans;Blithering Badgers;win
@@ -109,7 +109,7 @@ class TournamentTest < Minitest::Test
   end
 
   def test_there_can_be_more_than_two_teams
-    skip
+    # skip
     input = <<~INPUT
     Allegoric Alaskans;Blithering Badgers;win
     Blithering Badgers;Courageous Californians;win
@@ -127,7 +127,7 @@ class TournamentTest < Minitest::Test
   end
 
   def test_typical_input
-    skip
+    # skip
     input = <<~INPUT
     Allegoric Alaskans;Blithering Badgers;win
     Devastating Donkeys;Courageous Californians;draw
@@ -149,7 +149,7 @@ class TournamentTest < Minitest::Test
   end
 
   def test_incomplete_competition_not_all_pairs_have_played
-    skip
+    # skip
     input = <<~INPUT
     Allegoric Alaskans;Blithering Badgers;loss
     Devastating Donkeys;Allegoric Alaskans;loss
@@ -169,7 +169,7 @@ class TournamentTest < Minitest::Test
   end
 
   def test_ties_broken_alphabetically
-    skip
+    # skip
     input = <<~INPUT
     Courageous Californians;Devastating Donkeys;win
     Allegoric Alaskans;Blithering Badgers;win
