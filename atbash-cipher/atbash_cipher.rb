560a1f44c19f83052d344dbdd50dc70bc420f5dc
Lucien Najev <luciennajev@hotmail.com>
2020-06-04 18:16:54 +0100

    new ex

--- atbash-cipher/atbash_cipher.rb
+++ atbash-cipher/atbash_cipher.rb
@@ -1,6 +1,23 @@
 module Atbash
+  ALPHABET = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z].freeze
 
   def self.encode(plaintext)
-    'bvh'
+    sanitize(plaintext)
+      .map { |letter| translate(letter, ALPHABET) }
+      .join('')
+      .scan(/.{1,5}/)
+      .join(' ')
+  end
+
+  def self.decode(ciphertext)
+    sanitize(ciphertext).map { |letter| translate(letter, ALPHABET.reverse) }.join('')
+  end
+
+  def self.sanitize(string)
+    string.downcase.gsub(/[^a-z0-9]/, '').split('')
+  end
+
+  def self.translate(letter, key)
+    key.include?(letter) ? key.reverse[key.index(letter)] : letter
   end
 end
