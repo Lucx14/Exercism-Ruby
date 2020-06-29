0a222efe766f81515bf2f5974ef53f31c8a767a9
Lucien Najev <luciennajev@hotmail.com>
2020-06-29 11:54:44 +0100

    sub cipher done

--- simple-cipher/simple_cipher.rb
+++ simple-cipher/simple_cipher.rb
@@ -1,17 +1,29 @@
 class Cipher
   attr_reader :key
 
+  ALPHABET = 'abcdefghijklmnopqrstuvwxyz'.freeze
+
   def initialize(key = 'aaaaaaaaaaaaaaaaaa')
     validate_key(key)
     @key = key
   end
 
   def encode(plaintext)
-    plaintext
+    zz = plaintext.split('').each_with_index.map { |_, i| ALPHABET.index(key[i]) }
+
+    xx = plaintext.split('').each_with_index.map do |letter, i|
+      ALPHABET[(ALPHABET.index(letter) + zz[i]) % 26]
+    end.join
+    xx
   end
 
   def decode(ciphertext)
-    ciphertext
+    zz = ciphertext.split('').each_with_index.map { |_, i| ALPHABET.index(key[i]) }
+
+    xx = ciphertext.split('').each_with_index.map do |letter, i|
+      ALPHABET[ALPHABET.index(letter) - zz[i]]
+    end.join('')
+    xx
   end
 
   private
