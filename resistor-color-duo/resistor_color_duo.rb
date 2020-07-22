55d999be8d9b6f646163ecb69357fe429a440c12
Lucien Najev <luciennajev@hotmail.com>
2020-07-22 23:03:15 +0100

    refactor

--- resistor-color-duo/resistor_color_duo.rb
+++ resistor-color-duo/resistor_color_duo.rb
@@ -1,23 +1,12 @@
-module ResistorColorDuo
+# frozen_string_literal: true
 
-  BANDS = {
-    Black: 0,
-    Brown: 1,
-    Red: 2,
-    Orange: 3,
-    Yellow: 4,
-    Green: 5,
-    Blue: 6,
-    Violet: 7,
-    Grey: 8,
-    White: 9
-  }
+# Resistor Duo
+module ResistorColorDuo
+  BANDS = %w[black brown red orange yellow green blue violet grey white].freeze
 
   def self.value(pair)
-    color_1 = BANDS[pair.first.capitalize.to_sym].to_s
-    color_2 = BANDS[pair.last.capitalize.to_sym].to_s
-    val = color_1 + color_2
-    val.to_i
+    (
+      BANDS.index(pair[0]).to_s + BANDS.index(pair[1]).to_s
+    ).to_i
   end
-
 end
