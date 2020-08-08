0c5b0f1cc101ebed70a1082bdfbad300b72e3f4a
Lucien Najev <luciennajev@hotmail.com>
2020-08-08 12:51:17 +0100

    new ex

--- alphametics/alphametics.rb
+++ alphametics/alphametics.rb
@@ -3,6 +3,14 @@
 # Alphametics
 module Alphametics
   def self.solve(puzzle)
+    # assume all the sum numbers are 9 what is the max
+    # "I" = 9
+    # "B" = 9
+    # 9 + 99 = 108
+    # I must = 1
+    # 1 + 99 = 100
+    # L must = 0
+
     p puzzle
     { 'B' => 9, 'I' => 1, 'L' => 0 }
   end
