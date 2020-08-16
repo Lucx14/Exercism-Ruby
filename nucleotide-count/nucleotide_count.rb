8014ef94e01a854eeec91848af8a67afac732bcf
Lucien Najev <luciennajev@hotmail.com>
2020-08-16 13:11:34 +0100

    new ex

--- nucleotide-count/nucleotide_count.rb
+++ nucleotide-count/nucleotide_count.rb
@@ -2,27 +2,24 @@
 
 # Nucleotide
 class Nucleotide
-  attr_reader :histogram
+  VALID_NUCLEOTIDES = 'ATCG'
+  attr_reader :dna
 
   def initialize(dna)
-    raise ArgumentError if dna.match(/[^CAGT]/)
-
-    @histogram = build_histogram(dna)
+    @dna = dna
   end
 
   def self.from_dna(dna)
+    raise ArgumentError if dna.match(/[^#{VALID_NUCLEOTIDES}]/)
+
     new(dna)
   end
 
   def count(nucleotide)
-    histogram[nucleotide]
+    dna.count(nucleotide)
   end
 
-  private
-
-  def build_histogram(dna)
-    histogram = { 'A' => 0, 'T' => 0, 'C' => 0, 'G' => 0 }
-    dna.chars.each { |nucleotide| histogram[nucleotide] += 1 }
-    histogram
+  def histogram
+    VALID_NUCLEOTIDES.chars.map { |n| [n, count(n)] }.to_h
   end
 end
