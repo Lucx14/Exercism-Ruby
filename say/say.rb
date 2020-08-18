485f54abfc54038f703c55dcd4af37f70fa454ca
Lucien Najev <luciennajev@hotmail.com>
2020-08-18 12:49:13 +0100

    new ex

--- say/say.rb
+++ say/say.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
+# Say
 class Say
-  MAP = {
+  MAX_NUMBER = 999_999_999_999
+  TRANSLATION = {
     0 => [nil, nil, nil],
     1 => %w[one ten eleven],
     2 => %w[two twenty twelve],
@@ -10,58 +14,64 @@ class Say
     7 => %w[seven seventy seventeen],
     8 => %w[eight eighty eighteen],
     9 => %w[nine ninety nineteen]
-  }
+  }.freeze
 
   attr_reader :number
   def initialize(number)
-    raise ArgumentError if number < 0 || number > 999999999999
+    raise ArgumentError if number.negative? || number > MAX_NUMBER
+
     @number = number
   end
 
   def in_english
-    return "zero" if number == 0
+    return 'zero' if number.zero?
 
-    nums = number.to_s.chars.map(&:to_i)
-    split_num = nums.reverse.each_slice(3).to_a.map(&:reverse).map(&:join).map(&:to_i).reverse
-    yy = split_num.reverse.zip(['thousand', 'million', 'billion'])
-    zz = yy.map do |data|
-      if data[0].to_s.chars.length == 3
-        [format_3(data[0]), data[1]]
-      else
-        [format_2(data[0]), data[1]]
-      end
-    end
-    qq = zz.flatten.reverse.drop(1)
-    qq.each_slice(2).to_a.map { |data| data unless data[0] == nil }.join(' ').strip
-    # p zz.flatten.reverse.drop(1).join(' ').strip
+    compact_join(chunks(digits(number), 3)
+      .zip(%w[thousand million billion])
+      .map { |data| digits(data[0]).length == 3 ? [format_3(data[0]), data[1]] : [format_2(data[0]), data[1]] }
+      .flatten
+      .reverse
+      .drop(1)
+      .each_slice(2)
+      .map { |data| data unless data[0].nil? })
   end
 
-  def format_3(num)
-    nums = num.to_s.chars.map(&:to_i)
-    split_num = nums.reverse.each_slice(2).to_a.map(&:reverse).map(&:join).map(&:to_i).reverse
-    split_num
-      .reverse
+  def format_3(number)
+    compact_join(chunks(digits(number), 2)
       .zip(['hundred'])
       .map { |data| [format_2(data[0]), data[1]] }
       .flatten
-      .compact
-      .reverse
-      .join(' ')
+      .reverse)
   end
 
-  def format_2(num)
-    raise ArgumentError if num.to_s.chars.length > 2
-
-    result = []
-    if num < 10
-      MAP[num][0]
-    elsif num > 10 && num < 20
-      MAP[num.to_s.chars[1].to_i][2]
-    elsif num.to_s.chars.length == 2 && num.to_s.chars[1] != '0'
-      num.to_s.chars.reverse.each_with_index.map { |n, i| result << MAP[n.to_i][i] }
-      result.reverse.join('-')
-    elsif num.to_s.chars.length == 2 && num.to_s.chars[1] == '0'
-      MAP[num.to_s.chars[0].to_i][1]
+  def format_2(number)
+    if number < 10
+      TRANSLATION[number][0]
+    elsif number.between?(11, 19)
+      TRANSLATION[digits(number)[1]][2]
+    elsif (number % 10).zero?
+      TRANSLATION[digits(number)[0]][1]
+    elsif number % 10 != 0
+      digits(number).reverse.each_with_index.map { |num, i| TRANSLATION[num][i] }.reverse.join('-')
     end
+  end
+
+  def digits(number)
+    number.to_s.chars.map(&:to_i)
+  end
+
+  def chunks(array_of_numbers, slice_size)
+    array_of_numbers
+      .reverse
+      .each_slice(slice_size)
+      .map(&:reverse)
+      .map(&:join)
+      .map(&:to_i)
+  end
+
+  def compact_join(array)
+    array
+      .compact
+      .join(' ')
   end
 end
