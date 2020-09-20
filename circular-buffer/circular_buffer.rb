1682135e11e9504e38f2a92c74348448921f366c
Lucien Najev <luciennajev@hotmail.com>
2020-09-20 13:21:12 +0100

    new ex

--- circular-buffer/circular_buffer.rb
+++ circular-buffer/circular_buffer.rb
@@ -3,14 +3,43 @@
 # Circular Buffer
 class CircularBuffer
   class BufferEmptyException < StandardError; end
+  class BufferFullException < StandardError; end
 
-  attr_reader :num
+  attr_reader :size, :buffer
 
-  def initialize(num)
-    @num = num
+  def initialize(size)
+    @size = size
+    @buffer = (1..size).map { [] }
+  end
+
+  def write(data)
+    raise CircularBuffer::BufferFullException if empty_nodes.zero?
+
+    buffer.detect(&:empty?) << data
   end
 
   def read
-    raise CircularBuffer::BufferEmptyException
+    raise CircularBuffer::BufferEmptyException if empty_nodes == size
+
+    buffer.rotate!
+    buffer.last.pop
+  end
+
+  def write!(data)
+    return write(data) unless empty_nodes.zero?
+
+    buffer.rotate!
+    buffer.last.pop
+    buffer.last << data
+  end
+
+  def clear
+    buffer.each(&:pop)
+  end
+
+  private
+
+  def empty_nodes
+    buffer.select(&:empty?).size
   end
 end
