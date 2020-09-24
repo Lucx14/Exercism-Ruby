feaac4ae9274f758a54974576e3f51bcf185e03b
Lucien Najev <luciennajev@hotmail.com>
2020-09-24 17:53:13 +0100

    new ex

--- pascals-triangle/pascals_triangle.rb
+++ pascals-triangle/pascals_triangle.rb
@@ -9,19 +9,27 @@ class Triangle
   end
 
   def rows
-    result = [[1]]
-    starting_point = 010
-    p number_of_rows
+    result = []
+    (1..number_of_rows).map do |row|
+      batch = []
+      until batch.length == row
+        batch << 1
+        break if result.empty?
 
+        if result.length > 1
+          sum_nums(result[row - 2]).each { |num| batch << num }
+          batch << 1
+        end
+      end
 
-    until result.length == number_of_rows - 1 do
-      next_batch = []
+      result << batch
     end
-    p (1..number_of_rows).map do |row|
-      
-
-    end
+    result
   end
 
+  def sum_nums(arr)
+    arr.each_with_index.map do |el, i|
+      el + arr[i + 1] if i < arr.length - 1
+    end.compact
+  end
 end
-# the row number == the length of the row
