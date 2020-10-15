58b9532892073712d72e51368cff754dab2bd2d3
Lucien Najev <luciennajev@hotmail.com>
2020-10-15 19:27:51 +0100

    new ex

---
+++ two-bucket/two_bucket.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Two Buckets
+class TwoBuckets
+
+  def initialize(size_of_bucket_one, size_of_bucket_two, desired_volume, bucket_to_fill_first)
+
+
+  end
+
+  def moves; end
+  def goal_bucket; end
+  def other_bucket; end
+end
+
+# the only valid moves are:
+# pouring from one bucket to another
+# emptying one bucket and doing nothing to the other
+# filling one bucket and doing nothing to the other
